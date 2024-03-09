ff2973959a94815a3d4da53f64c7b39e842a3a88
srikanth nr <sriknr@gmail.com>
2024-03-09 14:35:31 +0530

    Shopping MVC project with hardcoded product list

---
+++ Shopping.Client/Program.cs
@@ -0,0 +1,35 @@
+using HealthChecks.UI.Client;
+using Shopping.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services
+    .AddHealthChecks()
+    .AddCheck<CustomHealthCheck>(nameof(CustomHealthCheck));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHealthChecks("/health", new()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.Run();
